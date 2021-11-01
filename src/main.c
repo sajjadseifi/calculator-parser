2d602ff12f8124eb0aae151d75ab12b77a187218
Sajjad Seifi <s.09334497255@gmail.com>
2021-11-01 12:51:42 -0400

    complet main function for compiler and interpret

--- src/main.c
+++ src/main.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
+#include "include/parser.h"
+#include "include/backend.h"
+#include "include/error.h"
 
-int main()
+int main(int argc, char **argv)
 {
-    printf("process...");
+    AST_node *icode;
+    int stauts = 0;
+    if(argc >1 && argv[1] == 1) 
+    {
+        stauts = 1;
+    }
+    if(!parse_program(&icode))
+    {
+        printf("no top-level experssion\n");
+        return 1;
+    }
+
+    process(icode,stauts);
+        
     return 0;
 }
